fda88cd1063d6b323e1b0d847d5ac8eb0ef13067
alexisbh <alexisbharris@gmail.com>
2019-05-02 15:03:51 -0400

    finished view

---
+++ src/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div  class="ToDoItem">
+    <p class="ToDoItem-Text">{{todo.text}}</p>
+    <div class="ToDoItem-Delete" @click="deleteItem(todo)">-
+    <input type="text" v-model="todo"  v-on:keyup.enter="createNewToDoItem"/>
+    <div  class="ToDo-Add"  @click="createNewToDoItem()">+</div>    
+	</div>
+  </div>
+</template>
+
+<script>
+  export  default  {
+  name: "to-do-item",
+    props: ['todo'],
+    methods: {
+  deleteItem(todo) {
+    this.$emit('delete', todo)
+  }
+}
+  }
+</script>
